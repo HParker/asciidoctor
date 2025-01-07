7ef0a6ca57a20e7e6ea496a56d753f58b127f01a
Adam Hess <hparker@github.com>
2025-01-07 11:30:33 -0800

    Remove hash merge extension
    Since the minimum Ruby version is now 2.7, we no longer need this hash extension to add the merge method

--- lib/asciidoctor/core_ext.rb
+++ lib/asciidoctor/core_ext.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'core_ext/nil_or_empty'
-require_relative 'core_ext/hash/merge'
 if RUBY_ENGINE == 'opal'
   require_relative 'core_ext/match_data/names'
 else
